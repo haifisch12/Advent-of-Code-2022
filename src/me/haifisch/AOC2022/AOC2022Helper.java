9c6254aa836874c3848bd5198945e8fd861762da
Niklas Schild <niklas.schild@hotmail.com>
2022-12-05 22:54:09 +0100

    removed unused import :)

--- src/me/haifisch/AOC2022/AOC2022Helper.java
+++ src/me/haifisch/AOC2022/AOC2022Helper.java
@@ -6,7 +6,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
 import java.util.function.Function;
 
 public final class AOC2022Helper<T> {
